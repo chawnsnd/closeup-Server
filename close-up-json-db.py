bd8746ccc0eb6eaa1fad4862b5b6a1a58c3a196e
bkh_linux_desktop <bkh751@gmail.com>
2018-11-10 15:58:54 +0900

    API  구현 (DB 관리 + 추천) 1.insert_pois(pois,category) 2.query_pois(keyWord,col) 3.update_star(id,starPoint) 4.query_poi(id) 5.query_square_bound(squareBound)
    1) 디비에 저장용 검사 :
    2) 리스트 검색 :
    3) 별점매기기 :
    4) POI하나 가져오기 :
    5) 최적장소 추천 ( 여기서는 사각형 바운드 안의 poi set 들을 query):

--- close-up-json-db.py
+++ close-up-json-db.py
@@ -35,7 +35,56 @@ else:
 # collist = mydb.list_collection_names()
 # print(mycol.find_one())
 
-def state_event(squareBound):
+def insert_pois(pois,category):
+    global mycol 
+    for poi in pois:
+        criteria = {"id":poi['id']}
+        setCategory= {"$set" : {"categories":category}}
+        addCategory = {"$push": {"categories":category}}
+        if mycol.find(criteria).limit(1).count():
+            if poi['categories'] is None: 
+                mycol.update_one(criteria,setCategory)
+            else:
+                mycol.update_one(criteria,addCategory)
+            continue
+        mycol.insert(poi)
+    return False
+
+
+def query_pois(keyWord,col):
+    query = {"$or":[{"name":keyWord},{"address_big":keyWord},
+                    {"address_mid":keyWord},{"address_small":keyWord},
+                    {"address_detail":keyWord},{"address_road":keyWord}]}
+    #Sample code for querynig included str 
+    # query = {"$or":[{"name":/searchStr/},{"address_big":/searchStr/}]}
+    pois = col.find({},query)
+    return dumps(pois)
+
+
+def update_star(id,starPoint):
+    global mycol
+    starCount = 1
+    criteria = {"id":id}
+    setStar = {"$set" : {"starPoint":starPoint,"starCount":starCount}}
+    poi = mycol.find(criteria)
+    if poi['star'] is None:
+        mycol.update_one(criteria,setStar)
+        return True
+    else :
+        starCount+=poi['starCount']
+        starPoint /=starCount
+        mycol.update_one(criteria,setStar)
+        return True
+
+    return False
+
+def query_poi(id):
+    global mycol
+    query = {"id":id}
+    poi = mycol.find(query).limit(1)
+    return dumps(poi)
+
+def query_square_bound(squareBound):
     global mycol
     maxLat = squareBound[0]
     minLat = squareBound[1]
@@ -43,36 +92,41 @@ def state_event(squareBound):
     minLon = squareBound[3]
     myquery = {"lat":{"$gt":minLat, "$lt":maxLat},"lon":{"$gt":minLon,"$lt":maxLon}}
     result = mycol.find(myquery)
-    # for poi in result:
-    #     # print(poi)
-    #     markers.append([poi['lon'],poi['lat']])
-    # # return json.dumps({'type': 'state', **STATE})
     return dumps(result)
+
+#이름, 큰 ,주소, 중간주소, 잗은 디테일소, 카테고리들, 도로명주소 
+def query_pois_all(keyWord):
+    global mycol 
+    query = {"$or":[{"name":keyWord},{"address_big":keyWord},
+                    {"address_mid":keyWord},{"address_small":keyWord},
+                    {"address_detail":keyWord},{"address_road":keyWord}]}
+    #Sample code for querynig included str 
+    # query = {"$or":[{"name":/searchStr/},{"address_big":/searchStr/}]}
+    pois = mycol.find({},query)
+    return dumps(pois)
+
+
+
 
 
 async def notify_state(squareBound):
     if USERS:       # asyncio.wait doesn't accept an empty list
-        message = state_event(squareBound)
-        # for user in USERS:
-        #     user.send(message) 
+        message = query_square_bound(squareBound)
         await asyncio.wait([user.send(message) for user in USERS])
-
 
 
 async def register(websocket):
     USERS.add(websocket)
 
-
 async def unregister(websocket):
     USERS.remove(websocket)
 
-
-async def counter(websocket, path):
+async def serve_api(websocket, path):
     global mycol
     # register(websocket) sends user_event() to websocket
     await register(websocket)
     try:
-        # await websocket.send(state_event())
+        # await websocket.send(query_square_bound())
         async for message in websocket:
             data = json.loads(message)
             maxLat = 0
@@ -101,19 +155,9 @@ async def counter(websocket, path):
             squareBound = [maxLat,minLat,maxLon,minLon]
             await notify_state(squareBound)
             
-            # SAMPLE CODE
-            # mycol.insert_many(data['persons'])
-            # mycol.insert_many(data['pois'])
-            # if data['a_lonlat'] != None and data['b_lonlat'] != None:
-            #     LONLAT['a_lonlat'] = data['a_lonlat']
-            #     LONLAT['b_lonlat'] = data['b_lonlat']
-            #     await notify_state()
-            # else:
-            #     logging.error(
-            #         "unsupported event: {}", data)
     finally:
         await unregister(websocket)
 
 asyncio.get_event_loop().run_until_complete(
-    websockets.serve(counter, 'localhost', 6789))
+    websockets.serve(serve_api, 'localhost', 6789))
 asyncio.get_event_loop().run_forever()
