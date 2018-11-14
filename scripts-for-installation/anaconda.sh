a9e09c81f51c798c1b14c282d3179b60c5fbd187
bkh_linux_desktop <bkh751@gmail.com>
2018-11-15 02:03:40 +0900

    sh script update

--- scripts-for-installation/anaconda.sh
+++ scripts-for-installation/anaconda.sh
@@ -17,32 +17,6 @@ sha256sum Anaconda3-5.0.1-Linux-x86_64.sh
 echo " run the installer"
 bash Anaconda3-5.0.1-Linux-x86_64.sh
 
-echo "source to activate conda"
-source ~/.bashrc
-
-echo " verify installation"
-conda list
-
-echo "check what python versions are available "
-conda search "^python$"
-
-echo "make your python env  (for example here, python3.6_ana is the name of the env)"
-conda create --name python3.6_ana python=3.6
-
-echo "activate your env"
-source activate python3.6_ana
-
-echo "install websockets"
-conda install -c conda-forge websockets
-
-echo "install numpy"
-conda install numpy
-
-echo "install scikit-learn"
-conda install -c anaconda scikit-learn 
-
-echo "install matplotlib"
-conda install matplotlib
 
 echo "==========================anaconda installation completed============"
 
