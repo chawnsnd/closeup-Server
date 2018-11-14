63c276febde14ff6f6ee69d88c2cbd19d0726c4b
bkh_linux_desktop <bkh751@gmail.com>
2018-11-15 02:06:26 +0900

    anaconda module installation added

---
+++ scripts-for-installation/anaconda-python-module.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Apache License 2.0
+# Copyright (c) 2018, CloseUp CO., LTD.
+
+echo "===================[Anaconda3 module installation ]======================="
+
+echo "source to activate conda"
+source ~/.bashrc
+
+echo " verify installation"
+conda list
+
+echo "check what python versions are available "
+conda search "^python$"
+
+echo "make your python env  (for example here, python3.6_ana is the name of the env)"
+conda create --name python3.6_ana python=3.6
+
+echo "activate your env"
+source activate python3.6_ana
+
+echo "install websockets"
+conda install -c conda-forge websockets
+
+echo "install numpy"
+conda install numpy
+
+echo "install scikit-learn"
+conda install -c anaconda scikit-learn 
+
+echo "install matplotlib"
+conda install matplotlib
+
+echo "==========================conda module installation completed============"
+
+
+exit 0
