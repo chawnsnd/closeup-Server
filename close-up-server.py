c1a9d048a0ed6b8690978506db1cefb5c89a2d38
bkh-laptop-inferior <bkh751@gmail.com>
2018-11-21 17:18:35 +0900

    /api 추가

--- close-up-server.py
+++ close-up-server.py
@@ -15,9 +15,9 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app, resources={
-  r"/v1/*": {"origin": "*"},
-  r"/api/*": {"origin": "*"},
+  r"/*": {"origin": "*"},
 })
+
 # START DB
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["CloseUpDB"]
@@ -32,31 +32,30 @@ else:
 
 
 
-
-@app.route("/pois", methods=["POST"])
+@app.route("/api/pois", methods=["POST"])
 def insertPois():
     req = request.json
     res = insert_pois(req['pois'],req['categories'])
     return jsonify(res)
 
 
-@app.route("/pois", methods=["GET"])
+@app.route("/api/pois", methods=["GET"])
 def getPois():
     req = request.json
     res = query_pois(req['keyWord'],req['count'],req['page'],mycol)
     return jsonify(res)
 
-@app.route("/pois/<poiId>/<starPoint>", methods=["PUT"])
+@app.route("/api/pois/<poiId>/<starPoint>", methods=["PUT"])
 def updateStar(poiId,starPoint):
     res = update_star(poiId,starPoint)
     return jsonify(res)
 
-@app.route("/pois/<poiId>", methods=["GET"])
+@app.route("/api/pois/<poiId>", methods=["GET"])
 def getPoi(poiId):
     res = query_poi(poiId)
     return jsonify(res)
 
-@app.route("/recommendPois", methods=["GET"])
+@app.route("/api/recommendPois", methods=["GET"])
 def recommendPois():
     req = request.json
     res = recommend_api(req['people_chosen'],req['keyWord'])
