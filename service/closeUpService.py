6c555c5f02fa14953c238fcff66332a0e5036003
bkh_linux_desktop <bkh751@gmail.com>
2018-11-24 19:33:12 +0900

    AWS 사각형 바운더리 버젼

--- service/closeUpService.py
+++ service/closeUpService.py
@@ -32,31 +32,10 @@ def recommendPois(keyWord, people_chosen):
         newPeople.append(literal_eval(person))
     peopleList = list(newPeople)
 
-    # 세사람이랑 중심간 평균거리 1/2 만큼 중심으로부터의 좌표만 쏘는거
-    sumLat, sumLon = 0,0
-    for person in peopleList:
-        sumLat += person['lat']
-        sumLon += person['lon']
-    midLat = sumLat/len(peopleList)
-    midLon = sumLon/len(peopleList)
-    
-    avgDistFromMid=0
-    sumDistFromMid=0
-    for person in peopleList:
-        sumDistFromMid +=(person['lat'] - midLat)**2 + (person['lon'] - midLon)**2
-    avgDistFromMid = sumDistFromMid/len(peopleList)
-        
     query_poisList = list(query_square_bound_and_keyword(newPeople,keyWord))
-    nearMidPlaces=[]
-    for poi in query_poisList:
-        if (poi['lat'] - midLat)**2 + (poi['lon'] - midLon)**2 < (avgDistFromMid/4):
-            nearMidPlaces.append(poi)
 
-    # sorted(query_poisList, key=lambda poi: (midLat- poi['lat'])**2 +(midLon -poi['lon'])**2)
-    
     #알고리즘
-    recommendation = recommend_system(peopleList,nearMidPlaces)
-    # recommendation = recommend_system(peopleList,query_poisList)
+    recommendation = recommend_system(peopleList,query_poisList)
     pois = []
     for r in recommendation :
         poi = {}
